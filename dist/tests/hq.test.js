a6863f81744a693d4556d9dedc48ba84c1b13c01
Róisín Grannell <r.grannell2@gmail.com>
2020-11-17 20:32:48 +0000

    upgraded

--- dist/tests/hq.test.js
+++ dist/tests/hq.test.js
@@ -1,17 +1,13 @@
 import stream from 'stream';
-import child_process from 'child_process';
-const { spawn } = child_process;
+import execa from 'execa';
 const { Readable } = stream;
-const testHq = async () => {
-    const hq = spawn('./dist/src/cli.js', ['p']);
-    hq.stdin.write(`<html><p attr0=val0>content0</html>\n`);
-    hq.stdin.end();
-    hq.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-    hq.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-    console.log('x');
+const test = async () => {
+    const subprocess = execa('./dist/src/cli.js');
+    const readable = Readable.from([
+        `<html><p attr0=val0>content0</html>`
+    ]);
+    readable.pipe(subprocess.stdin);
+    const { stdout } = await subprocess;
+    console.log(stdout);
 };
-testHq();
+test();
--- tests/hq.test.ts
+++ tests/hq.test.ts
@@ -1,28 +1,22 @@
 
 import stream from 'stream'
-import child_process from 'child_process'
-const { spawn } = child_process
+import fetch from 'node-fetch'
+import execa from 'execa'
 import * as fs from 'fs'
 
 const { Readable } = stream
 
-const testHq = async () => {
-  const hq = spawn('./dist/src/cli.js', ['p'])
+const test = async () => {
+  const subprocess = execa('./dist/src/cli.js')
+  const readable = Readable.from([
+    `<html><p attr0=val0>content0</html>`
+  ])
 
-  hq.stdin.write(`<html><p attr0=val0>content0</html>\n`)
-  hq.stdin.end()
+  readable.pipe(subprocess.stdin as any)
 
-  hq.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`)
-  })
-
-  hq.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`)
-  })
-
-console.log('x')
-
+  const {stdout} = await subprocess
+  console.log(stdout)
 }
 
-testHq()
+test()
 
