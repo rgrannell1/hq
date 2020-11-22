8bfc1b0b02f2e97c88922a368cc9c4ef4069777f
Róisín Grannell <r.grannell2@gmail.com>
2020-11-22 17:41:32 +0000

    built tsc

--- src/cli.ts
+++ src/cli.ts
@@ -8,9 +8,14 @@ Name:
 
 Usage:
   hq <selector> [<url>]
+
 Description:
   hq selects HTML content using a standard JS selector and returns matching content in JSON format. This includes both the
   element's text-content and any HTML attributes that were set.
+
+Options:
+  <selector>    a valid JS element selector.
+  <url>         a URL to open directly via puppeteer. Optional. By default, stdin receives HTML from another tool.
 
 Examples:
   hq 'p'
